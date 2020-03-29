910c69ac2c796ef537b116570df25bbfb0370d39
Katarzyna Góźdź <k.j.gozdz@gmail.com>
2020-03-29 14:01:41 +0200

    Refactor of AmountWidget, Add BaseWidget.js

---
+++ src/js/components/BaseWidget.js
@@ -0,0 +1,52 @@
+export class BaseWidget {
+  constructor(wrapperElement, initialValue) {
+    const thisWidget = this;
+    thisWidget.dom = {};
+    thisWidget.dom.wrapper = wrapperElement;
+    thisWidget.correctValue = initialValue;
+  }
+
+  get value() {
+    const thisWidget = this;
+
+    return thisWidget.correctValue;
+  }
+
+  set value(assignedValue) {
+    const thisWidget = this;
+
+    const newValue = thisWidget.parseValue(assignedValue);
+
+    if (newValue != thisWidget.correctValue && thisWidget.isValid(newValue)) {
+      thisWidget.correctValue = newValue;
+      thisWidget.announce();
+    }
+
+    thisWidget.renderValue();
+  }
+  /* Parsowanie (zmiana) argumentu na liczbę */
+  parseValue(newValue) {
+    return parseInt(newValue);
+  }
+  /* Sprawdzenie czy wartość jest poprawna */
+  isValid(newValue) {
+    return !isNaN(newValue);
+  }
+  /* Renderowanie wartości widgetu */
+  renderValue() {
+    const thisWidget = this;
+
+    console.log('widget value', thisWidget.value);
+  }
+  /* Wywołanie eventu informującego o zmianie wartości */
+  announce() {
+    const thisWidget = this;
+
+    const event = new CustomEvent('updated', {
+      bubbles: true
+    });
+
+    thisWidget.dom.wrapper.dispatchEvent(event);
+  }
+
+}
