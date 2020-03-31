2e58020bc5a1496ec7c2a49a10cab85edf916241
Katarzyna Góźdź <k.j.gozdz@gmail.com>
2020-03-31 14:08:45 +0200

    Fix Booking part1

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -219,6 +219,8 @@ export class Booking {
         return response.json();
       }).then(function (parsedResponse) {
         console.log('parsedResponse', parsedResponse);
+        thisBooking.makeBooked(payload.date, payload.hour, payload.duration, payload.table);
+        thisBooking.updateDOM();
       });
   }
 }
